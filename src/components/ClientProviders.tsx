715afb9dd13cebca4a950fa19c14b3b36538451c
Anatoliy Marchuk <marchukt@gmail.com>
2026-01-09 16:14:25 -0500

    test: add AuthProvider

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -2,22 +2,25 @@
 
 import type { ReactNode } from 'react'
 import { ThemeProvider } from './ThemeProvider'
+import { AuthProvider } from './AuthProvider'
 
 interface ClientProvidersProps {
   children: ReactNode
 }
 
-// TEST 1: Just ThemeProvider
+// TEST 2: ThemeProvider + AuthProvider
 export function ClientProviders({ children }: ClientProvidersProps) {
   return (
     <ThemeProvider>
-      <div className="min-h-screen">
-        <main className="p-4">
-          <div className="max-w-7xl mx-auto">
-            {children}
-          </div>
-        </main>
-      </div>
+      <AuthProvider>
+        <div className="min-h-screen">
+          <main className="p-4">
+            <div className="max-w-7xl mx-auto">
+              {children}
+            </div>
+          </main>
+        </div>
+      </AuthProvider>
     </ThemeProvider>
   )
 }

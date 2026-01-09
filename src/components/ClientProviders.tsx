b2dd9541579f6470eff2fd3616fb2ac076343b4c
Anatoliy Marchuk <marchukt@gmail.com>
2026-01-09 16:16:24 -0500

    test: add CreditProvider

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -3,23 +3,26 @@
 import type { ReactNode } from 'react'
 import { ThemeProvider } from './ThemeProvider'
 import { AuthProvider } from './AuthProvider'
+import { CreditProvider } from './CreditProvider'
 
 interface ClientProvidersProps {
   children: ReactNode
 }
 
-// TEST 2: ThemeProvider + AuthProvider
+// TEST 3: ThemeProvider + AuthProvider + CreditProvider
 export function ClientProviders({ children }: ClientProvidersProps) {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <div className="min-h-screen">
-          <main className="p-4">
-            <div className="max-w-7xl mx-auto">
-              {children}
-            </div>
-          </main>
-        </div>
+        <CreditProvider>
+          <div className="min-h-screen">
+            <main className="p-4">
+              <div className="max-w-7xl mx-auto">
+                {children}
+              </div>
+            </main>
+          </div>
+        </CreditProvider>
       </AuthProvider>
     </ThemeProvider>
   )

c30bd3f2fedcebbfb0dca8fa60b0e93656a2bc3e
Anatoliy Marchuk <marchukt@gmail.com>
2026-01-09 16:20:41 -0500

    test: add TerminalProvider

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -5,25 +5,28 @@ import { ThemeProvider } from './ThemeProvider'
 import { TooltipProvider } from './ui/tooltip'
 import { AuthProvider } from './AuthProvider'
 import { CreditProvider } from './CreditProvider'
+import { TerminalProvider } from './terminal/TerminalProvider'
 
 interface ClientProvidersProps {
   children: ReactNode
 }
 
-// TEST 4: + TooltipProvider
+// TEST 5: + TerminalProvider
 export function ClientProviders({ children }: ClientProvidersProps) {
   return (
     <ThemeProvider>
       <TooltipProvider delayDuration={200}>
         <AuthProvider>
           <CreditProvider>
-            <div className="min-h-screen">
-              <main className="p-4">
-                <div className="max-w-7xl mx-auto">
-                  {children}
-                </div>
-              </main>
-            </div>
+            <TerminalProvider>
+              <div className="min-h-screen">
+                <main className="p-4">
+                  <div className="max-w-7xl mx-auto">
+                    {children}
+                  </div>
+                </main>
+              </div>
+            </TerminalProvider>
           </CreditProvider>
         </AuthProvider>
       </TooltipProvider>

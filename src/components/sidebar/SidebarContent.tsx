3353f6426df74ee384eaefa5671094983850f6c1
Anatoliy Marchuk <marchukt@gmail.com>
2026-01-09 16:54:04 -0500

    test: add header components

--- src/components/sidebar/SidebarContent.tsx
+++ src/components/sidebar/SidebarContent.tsx
@@ -1,11 +1,22 @@
 'use client'
 
-// MINIMAL TEST VERSION - incrementally add components back
+// TEST: Adding header components
+import UserInfo from './UserInfo'
+import ThemeToggle from '@/components/ThemeToggle'
+import { CompactStatusBar } from './CompactStatusBar'
 
 export function SidebarContent() {
   return (
     <div className="flex flex-col h-full">
-      <div className="text-sm">Sidebar Test - Minimal</div>
+      {/* Header: Compact status + theme toggle + user info */}
+      <div className="flex items-center justify-between mb-4">
+        <CompactStatusBar />
+        <div className="flex items-center gap-1">
+          <ThemeToggle size={16} className="p-1.5" />
+          <UserInfo compact />
+        </div>
+      </div>
+      <div className="text-sm">Sidebar Test - Header added</div>
     </div>
   )
 }

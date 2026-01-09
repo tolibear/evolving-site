898cb5efe6076162e3fbd260ff69496db000dc8a
Anatoliy Marchuk <marchukt@gmail.com>
2026-01-09 16:10:18 -0500

    test: add ThemeProvider only

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,24 +1,23 @@
 'use client'
 
 import type { ReactNode } from 'react'
+import { ThemeProvider } from './ThemeProvider'
 
 interface ClientProvidersProps {
   children: ReactNode
 }
 
-// TEMPORARY: Completely stripped down - no providers, no client features
-// Just to test if the base page loads
+// TEST 1: Just ThemeProvider
 export function ClientProviders({ children }: ClientProvidersProps) {
   return (
-    <div className="min-h-screen bg-white dark:bg-neutral-900">
-      <main className="p-4">
-        <div className="max-w-7xl mx-auto">
-          {children}
-        </div>
-      </main>
-      <div className="fixed bottom-4 right-4 bg-yellow-100 dark:bg-yellow-900 p-4 rounded shadow">
-        <p className="text-sm">Debug mode: All features disabled to test base render</p>
+    <ThemeProvider>
+      <div className="min-h-screen">
+        <main className="p-4">
+          <div className="max-w-7xl mx-auto">
+            {children}
+          </div>
+        </main>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
